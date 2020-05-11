cd23c7d55b04d7490ef77600d6d8f3235a0ba731
Gadjung <gadjung@gmail.com>
2020-05-11 17:46:16 +0200

    Tombstone and MetalNoUV shader update (#137)
    Updated MetalNoUV shader and material to match properties with MetalUV
    Extruded badge a bit to make it more distinct
    Added fade-out effect on tombstone badge:
    
    badge stil blinks
    emission decay factor based on the tombstone starting decayTimer value
    maximum emission decays to lower value the closer for tombstone to decay
    badge is still visible when few tick before decaying completely****

--- Assets/Scripts/Screeps3D/RoomObjects/Views/TombstoneView.cs
+++ Assets/Scripts/Screeps3D/RoomObjects/Views/TombstoneView.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Common;
 using Screeps3D.Effects;
+using Screeps_API;
 using UnityEngine;
 
 namespace Screeps3D.RoomObjects.Views
@@ -16,16 +17,32 @@ namespace Screeps3D.RoomObjects.Views
         private Vector3 _posRef;
         private Tombstone _tombstone;
         private IEnumerator _blink;
+        private float _emissionDecayFactor;
         private float _nextBlink;
         private bool _blinking;
+        private long _maxEmission = 8;
+        private long _minEmission = 2;
+        private float _currentEmission;
+        private long _lastTickUpdate;
+        private long _loadTick;
+
 
         internal override void Load(RoomObject roomObject)
         {
-            base.Load(roomObject);
+            base.Load(roomObject);            
             _tombstone = roomObject as Tombstone;
+            long now = ScreepsAPI.Time;
+
+            long deathNowDiff = now - _tombstone.DeathTime;
+
+            _emissionDecayFactor = (float) (_maxEmission - _minEmission) / (deathNowDiff + (long)_tombstone.NextDecayTime - now);
+            _currentEmission = _maxEmission - deathNowDiff * _emissionDecayFactor;
+            _lastTickUpdate = now;
+
+            // Base setup
             _body.materials[1].SetColor("EmissionColor", new Color(0.7f, 0.7f, 0.7f, 1f));
             _body.materials[1].SetTexture("EmissionTexture", _tombstone?.Owner?.Badge);
-            _body.materials[1].SetFloat("EmissionStrength", 5f);
+            _body.materials[1].SetFloat("EmissionStrength", _currentEmission);
 
             _rotTarget = transform.rotation;
             _posTarget = roomObject.Position;
@@ -58,35 +75,33 @@ namespace Screeps3D.RoomObjects.Views
                 }
                 return;
             }
-
-            if(_blinking || Time.time < _nextBlink ) {
-                return;
+            // if tick changed
+            if(_lastTickUpdate != ScreepsAPI.Time) {
+                _currentEmission = _body.materials[1].GetFloat("EmissionStrength") - _emissionDecayFactor;
+                _lastTickUpdate = ScreepsAPI.Time;
             }
-            
-            _blink = Blink();
-            StartCoroutine(_blink);
-            //transform.localPosition = Vector3.SmoothDamp(transform.localPosition, _posTarget, ref _posRef, .5f);
-            //_rotationRoot.transform.rotation = Quaternion.Slerp(_rotationRoot.transform.rotation, _tombstone.Rotation, Time.deltaTime * 5);
+
+            if(!_blinking ) {
+                _blink = Blink();
+                StartCoroutine(_blink);
+            } 
         }
 
         private IEnumerator Blink()
         {
-            float min = 2f;
-            float max = 8f;
             _blinking = true;
-            while (_body.materials[1].GetFloat("EmissionStrength") < max)
+            while (_body.materials[1].GetFloat("EmissionStrength") < _currentEmission)
             {
                 _body.materials[1].SetFloat("EmissionStrength", _body.materials[1].GetFloat("EmissionStrength") + 0.1f);
                 yield return null;
             }
-            while (_body.materials[1].GetFloat("EmissionStrength") > min)
+            while (_body.materials[1].GetFloat("EmissionStrength") > _minEmission)
             {
                 _body.materials[1].SetFloat("EmissionStrength", _body.materials[1].GetFloat("EmissionStrength") - 0.1f);
                 yield return null;
             }
             _nextBlink = Time.time + Random.value + 1;
             _blinking = false;
-        }
-        
+        }        
     }
 }
