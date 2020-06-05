f01d077db7e13cb68130b6fe345a710a1842e489
thmsn <9084377+thmsndk@users.noreply.github.com>
2020-06-05 18:55:52 +0200

    Destroy singleton objects if they get recreated during scene load

--- Assets/Scripts/Common/BaseSingleton.cs
+++ Assets/Scripts/Common/BaseSingleton.cs
@@ -3,28 +3,37 @@ using UnityEngine;
 
 namespace Common
 {
-    public class BaseSingleton<T>: MonoBehaviour where T: Component
+    public class BaseSingleton<T> : MonoBehaviour where T : Component
     {
         [SerializeField] private bool _keepAlive = default;
         private static T _instance;
 
-        public static T Instance 
+        public static T Instance
         {
-            get {
+            get
+            {
                 if (_instance != null) return _instance;
                 _instance = FindObjectOfType<T>();
-                
+
                 if (_instance != null) return _instance;
 
                 throw new Exception(string.Format("expecting singleton of type {0} in scene", typeof(T)));
             }
         }
 
-        public virtual void Awake ()
+        public virtual void Awake()
         {
+            if (_instance != this && _instance != null)
+            {
+                Destroy(_instance);
+            }
+
             _instance = this as T;
+
             if (_keepAlive)
+            {
                 DontDestroyOnLoad(gameObject);
+            }
         }
     }
 }
