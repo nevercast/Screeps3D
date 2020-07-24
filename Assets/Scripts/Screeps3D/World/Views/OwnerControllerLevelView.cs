ed90895b65948c4ea9e2686f51e68a3128254d4a
thmsn <9084377+thmsndk@users.noreply.github.com>
2020-07-24 02:55:28 +0200

    Render roomname and controller level

--- Assets/Scripts/Screeps3D/World/Views/OwnerControllerLevelView.cs
+++ Assets/Scripts/Screeps3D/World/Views/OwnerControllerLevelView.cs
@@ -15,10 +15,12 @@ namespace Assets.Scripts.Screeps3D.World.Views
     public class OwnerControllerLevelView : MonoBehaviour, IWorldOverlayViewComponent
     {
         private OwnerControllerLevelData data;
-
+        [SerializeField] private GameObject _badgeScaledContent;
         [SerializeField] private Image _badge;
         [SerializeField] private Image _badgeMask;
         [SerializeField] private TMP_Text _username;
+        [SerializeField] private TMP_Text _roomName;
+        [SerializeField] private TMP_Text _controllerLevel;
 
         public void Init(object o)
         {
@@ -26,10 +28,36 @@ namespace Assets.Scripts.Screeps3D.World.Views
             // TODO: we need the room position and move it there
             //arcRenderer.point1.transform.position = Overlay.LaunchRoom.Position + new Vector3(25, 0, 25); // Center of the room, because we do not know where the nuke is, could perhaps scan for it and correct it?
             this.gameObject.transform.position = data.Room.Position + new Vector3(25, Constants.ShardHeight / 3, 25); // Center of the room
-
+            _roomName.text = data.RoomInfo.RoomName;
+            _controllerLevel.text = data.RoomInfo.Level?.ToString() ?? string.Empty;
             var user = data.RoomInfo.User;
+            float scale = 1f;
+            int level = data.RoomInfo.Level ?? 1;
+            if (data.RoomInfo.IsReserved)
+            {
+                level = 1;
+                //_badge.color.a = 0.5f; // TODO: make it alpha when reserved
+            }
+
+            switch (level)
+            {
+                case 7: scale = 0.8f; break;
+                case 6: scale = 0.6f; break;
+                case 5: scale = 0.5f; break;
+                case 4: scale = 0.4f; break;
+                case 3: scale = 0.3f; break;
+                case 2: scale = 0.2f; break;
+                case 1: scale = 0.1f; break;
+                default:
+                    break;
+            }
+
+            _badgeScaledContent.transform.localScale = new Vector3(scale, scale, 1f);
+
             if (user != null)
             {
+                _roomName.gameObject.SetActive(true);
+                _controllerLevel.gameObject.SetActive(true);
                 _badgeMask.gameObject.SetActive(true);
                 _badge.enabled = true;
                 // TODO: should we cache the sprite?
@@ -41,6 +69,8 @@ namespace Assets.Scripts.Screeps3D.World.Views
             }
             else
             {
+                _roomName.gameObject.SetActive(false);
+                _controllerLevel.gameObject.SetActive(false);
                 _badgeMask.gameObject.SetActive(false);
                 _username.enabled = false;
             }
