da74c0c7ee81383b95c3152b13eeeb794f877200
thmsn <9084377+thmsndk@users.noreply.github.com>
2020-08-28 21:14:19 +0200

    Crude connected ramparts with 4 sides

--- Assets/Scripts/Screeps3D/RoomObjects/Views/RampartView.cs
+++ Assets/Scripts/Screeps3D/RoomObjects/Views/RampartView.cs
@@ -1,12 +1,18 @@
 ﻿using Common;
 using Screeps_API;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Screeps3D.RoomObjects.Views
 {
     public class RampartView : MonoBehaviour, IObjectViewComponent
     {
-        [SerializeField] private Renderer rend = default;
+        [SerializeField] private Renderer _top = default;
+        [SerializeField] private Renderer _left = default;
+        [SerializeField] private Renderer _right = default;
+        [SerializeField] private Renderer _forward = default;
+        [SerializeField] private Renderer _backward = default;
         private Rampart _rampart;
 
         public void Init()
@@ -41,8 +47,55 @@ namespace Screeps3D.RoomObjects.Views
                 // Should only happen for ruins
                 //renderer.material.SetColor("_Color", Color.grey);
                 //renderer.material.SetColor("_EmissionColor", Color.grey);
-            }            
-            rend.materials[0].SetColor("EmissionColor", emissionColor);
+            }
+            _top.materials[0].SetColor("EmissionColor", emissionColor);
+            _left.materials[0].SetColor("EmissionColor", emissionColor);
+            _right.materials[0].SetColor("EmissionColor", emissionColor);
+            _forward.materials[0].SetColor("EmissionColor", emissionColor);
+            _backward.materials[0].SetColor("EmissionColor", emissionColor);
+
+            var forward = new Vector2(_rampart.X, _rampart.Y - 1);
+            var backward = new Vector2(_rampart.X, _rampart.Y + 1);
+            var left = new Vector2(_rampart.X - 1, _rampart.Y);
+            var right = new Vector2(_rampart.X + 1, _rampart.Y);
+
+            var renderForward = true;
+            var renderBackward = true;
+            var renderLeft = true;
+            var renderRight = true;
+
+            foreach (var o in _rampart?.Room.Objects.Values)
+            {
+                if (o.Type == Constants.TypeRampart)
+                {
+                    var position = new Vector2(o.X, o.Y);
+
+                    if (renderForward && position == forward)
+                    {
+                        renderForward = false;
+                    }
+
+                    if (renderBackward && position == backward)
+                    {
+                        renderBackward = false;
+                    }
+
+                    if (renderLeft && position == left)
+                    {
+                        renderLeft = false;
+                    }
+
+                    if (renderRight && position == right)
+                    {
+                        renderRight = false;
+                    }
+                }
+            }
+
+            _forward.enabled = renderForward;
+            _backward.enabled = renderBackward;
+            _left.enabled = renderLeft;
+            _right.enabled = renderRight;
         }
 
         public void Delta(JSONObject data)
